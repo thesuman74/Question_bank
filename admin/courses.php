0d645975f952f5410903f4a187d12ab642b4a5a7
suman <yes.u.man7@gmail.com>
2022-11-05 17:15:18 +0545

    comments added

--- admin/courses.php
+++ admin/courses.php
@@ -1,134 +1,131 @@
     <?php
-session_start();
+    session_start();
 
-if (!isset($_SESSION['fullname'])) {
-    echo "Please login ";
-    header('location:login.php');
-}
-?>
+    if (!isset($_SESSION['fullname'])) {
+        echo "Please login ";
+        header('location:login.php');
+    }
+    ?>
 
-<?php
-include('includes/header.php');
-include('includes/navbar.php');
-include('includes/links/links.php');
-include('connection.inc.php');
-
-
-$selectquery = "SELECT * FROM courses ";
-$showdata = mysqli_query($conn, $selectquery);
-
-if (isset($_POST['add_courses'])) {
-
-    echo'successful';
+    <?php
+    include('includes/header.php');
+    include('includes/navbar.php');
+    include('includes/links/links.php');
+    include('connection.inc.php');
 
 
+    $selectquery = "SELECT * FROM courses ";
+    $showdata = mysqli_query($conn, $selectquery);
 
-}
+    if (isset($_POST['add_courses'])) {
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <style>
-    img{
-      width: 300px;
-      height: 250px;
-      object-fit: cover;
+        echo 'successful';
     }
 
-    a {
-      align-items: center;
-      line-height: 28px;
-      justify-content: center;
+    ?>
 
-      --animate-duration: 800ms;
-      --animate-delay: 0.9s;
+    <!DOCTYPE html>
+    <html lang="en">
 
-    }
-  
-  </style>
-</head>
-<body>
-    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
 
+        <style>
+            img {
+                width: 300px;
+                height: 250px;
+                object-fit: cover;
+            }
 
-<main id="main">
-    <!-- ======= About Section ======= -->
+            a {
+                align-items: center;
+                line-height: 28px;
+                justify-content: center;
 
-    <h1 class="text-center text-capitalize pt-1 display-3 font-weight-normal">Courses</h1>
-    <hr class="w-75 mx-auto ">
+                --animate-duration: 800ms;
+                --animate-delay: 0.9s;
 
+            }
+        </style>
+    </head>
 
-    <div class="bg-white container mt-5 " style="width:auto; height: 600px;;">
-
-
-        <section id="courses" class="about">
-            <div class="container">
-                <h2>Available courses</h2>
-                <hr>
-                <div class="row">
-                   <?php 
-
-                   while ($re = mysqli_fetch_array($showdata)) {
-                   ?>  <div class="col-lg-4 col-sm-4 my-3 ">
-                    <a class="btn btn-primary animate__animated animate__pulse btn-lg d-flex  text-white " href="" role="button" style="width:200px; height: 150px;" >
-                        <?php echo strtoupper($re['name']);?> 
-                    </a>
-                    <!-- <button class="btn btn-danger mt-2">Delete</button> -->
-                </div>
-                <?php
-            }   
+    <body>
 
 
 
+        <main id="main">
+            <!-- ======= About Section ======= -->
 
-            ?>
-        </div>      
-    </div>
-    <hr>    
+            <h1 class="text-center text-capitalize pt-1 display-3 font-weight-normal">Courses</h1>
 
-    <h2>Add new courses</h2>
-    <!-- add course section  -->
-    <div class="add_courses">
-
-        <form action="add.php" method="POST"     >
-
-          <div class="input-group">
-            <input type="text" name="course" class="form-control" placeholder="Enter Course name" required>
-        </div>
-        <h2>Select the number of semesters</h2>
-        <select name="semester">
-            <option>select the number of semesters</option>
-            <option value="6" > 6</option>
-            <option value="8" > 8</option>
-        </select><br>
-
-        <input type="submit" name="add_courses" class="btn btn-primary mt-4" >       
-    </form>
-
-</div>
+            <div class="bg-white container mt-5 " style="width:auto; height: 600px;;">
 
 
-<!-- course add end here -->
+                <section id="courses" class="about">
+                    <div class="container">
+                        <h2>Available courses</h2>
+                        <hr>
+                        <div class="row">
+                            <?php
+
+                            while ($re = mysqli_fetch_array($showdata)) {
+                            ?> <div class="col-lg-4 col-sm-4 my-3 ">
+                                    <a class="btn btn-primary animate__animated animate__pulse btn-lg d-flex  text-white " href="" role="button" style="width:200px; height: 150px;">
+                                        <?php echo strtoupper($re['name']); ?>
+                                    </a>
+                                    <!-- <button class="btn btn-danger mt-2">Delete</button> -->
+                                </div>
+                            <?php
+                            }
 
 
-</div>
-
-</section>
 
 
-<!-- End About Section -->
+                            ?>
+                        </div>
+                    </div>
+                    <hr>
+
+                    <h2>Add new courses</h2>
+                    <!-- add course section  -->
+                    <div class="add_courses">
+
+                        <form action="add.php" method="POST">
+
+                            <div class="input-group">
+                                <input type="text" name="course" class="form-control" placeholder="Enter Course name" required>
+                            </div>
+                            <h2>Select the number of semesters</h2>
+                            <select name="semester">
+                                <option>select the number of semesters</option>
+                                <option value="6"> 6</option>
+                                <option value="8"> 8</option>
+                            </select><br>
+
+                            <input type="submit" name="add_courses" class="btn btn-primary mt-4">
+                        </form>
+
+                    </div>
 
 
-</body>
-</html>
+                    <!-- course add end here -->
 
-<?php
+
+            </div>
+
+            </section>
+
+
+            <!-- End About Section -->
+
+
+    </body>
+
+    </html>
+
+    <?php
     include('includes/scripts.php');
     include('includes/footer.php');
-?>
+    ?>
--- admin/data_entry.php
+++ admin/data_entry.php
@@ -6,8 +6,8 @@
 session_start();
 
 if (!isset($_SESSION['fullname'])) {
-  echo "Please login ";
-  header('location:login.php');
+    echo "Please login ";
+    header('location:login.php');
 }
 ?>
 
@@ -29,105 +29,105 @@ include('connection.inc.php');
     <div class="bg-white container mt-5 " style="width:auto; height: 500px;">
 
 
-<?php
-include 'connection.inc.php';
+        <?php
+        include 'connection.inc.php';
 
-$country = "SELECT * FROM courses";
-$county_qry = mysqli_query($conn, $country);
+        $country = "SELECT * FROM courses";
+        $county_qry = mysqli_query($conn, $country);
 
 
 
- ?>
+        ?>
 
-<div class="d-flex justify-content-center align-items-center">
-    <div class="container my-5">
-        <div class="card mx-auto">
+        <div class="d-flex justify-content-center align-items-center">
+            <div class="container my-5">
+                <div class="card mx-auto">
 
-            <form action="add.php" method="POST">
-            <div class="card-body">
-                <div class="input-group mb-3">
-                    <select class="form-select" id="country" name="course" required>
-                        <option selected disabled value="">Select course</option>
-                        <?php while ($row = mysqli_fetch_assoc($county_qry)) : ?>
-                            <option value="<?php echo $row['id']; ?>" > <?php echo $row['name']; ?> </option>
-                        <?php endwhile; ?>
-                    </select>
+                    <form action="add.php" method="POST">
+                        <div class="card-body">
+                            <div class="input-group mb-3">
+                                <select class="form-select" id="country" name="course" required>
+                                    <option selected disabled value="">Select course</option>
+                                    <?php while ($row = mysqli_fetch_assoc($county_qry)) : ?>
+                                        <option value="<?php echo $row['id']; ?>"> <?php echo $row['name']; ?> </option>
+                                    <?php endwhile; ?>
+                                </select>
+                            </div>
+                            <div class="input-group mb-3">
+                                <select required class="form-select" id="state" name='semester'>
+                                    <option selected disabled value="">Select Semester</option>
+                                </select>
+                            </div>
+                            <div class="input-group mb-3">
+                                <select class="form-select" id="city" name="subject" required>
+                                    <option selected disabled value="">Select Subject</option>
+                                </select>
+                            </div>
+                            <div class="input-group">
+                                <input type="text" name="link" class="form-control" placeholder="Enter google drive link" required>
+                            </div>
+
+
+                            <div class="input-group">
+                                <input type="file" name="file" class="form-control mt-3">
+                            </div>
+
+                        </div>
+
+                        <input type="submit" name="data_entry" class="btn btn-primary form-group ml-3">
+
+                    </form>
+
                 </div>
-                <div class="input-group mb-3">
-                    <select required class="form-select" id="state" name='semester'  >
-                        <option selected disabled value="">Select Semester</option>
-                    </select>
-                </div>
-                <div class="input-group mb-3">
-                    <select class="form-select" id="city" name="subject" required >
-                        <option selected disabled value="">Select Subject</option>
-                    </select>
-                </div>
-                <div class="input-group">
-                    <input type="text" name="link" class="form-control" placeholder="Enter google drive link" required  >
-                </div>
 
-
-                <div class="input-group">
-                    <input type="file" name="file" class="form-control mt-3" >
-                </div>
 
             </div>
-                
-                <input type="submit" name="data_entry" class="btn btn-primary form-group ml-3" > 
-
-            </form>
-
         </div>
 
+        <?php include 'includes/footer.php' ?>
 
-    </div>
-</div>
+        <script>
+            // County as course  State as semester
 
-<?php include 'includes/footer.php' ?>
-
-<script>
-    // County State
-
-    $('#country').on('change', function() {
-        var country_id = this.value;
-        console.log(country_id);
-        $.ajax({
-            url: 'ajax/state.php',
-            type: "POST",
-            data: {
-                country_data: country_id
-            },
-            success: function(result) {
-                $('#state').html(result);
-                // console.log(result);
-            }
-        })
-    });
-    // state city
-    $('#state').on('change', function() {
-        var state_id = this.value;
-        console.log(state_id);
-        $.ajax({
-            url: 'ajax/city.php',
-            type: "POST",
-            data: {
-                state_data: state_id
-            },
-            success: function(data) {
-                $('#city').html(data);
-                console.log(data);
-            }
-        })
-    });
-</script>
+            $('#country').on('change', function() {
+                var country_id = this.value;
+                console.log(country_id);
+                $.ajax({
+                    url: 'ajax/state.php',
+                    type: "POST",
+                    data: {
+                        country_data: country_id
+                    },
+                    success: function(result) {
+                        $('#state').html(result);
+                        // console.log(result);
+                    }
+                })
+            });
+            // state as semester city as subjects
+            $('#state').on('change', function() {
+                var state_id = this.value;
+                console.log(state_id);
+                $.ajax({
+                    url: 'ajax/city.php',
+                    type: "POST",
+                    data: {
+                        state_data: state_id
+                    },
+                    success: function(data) {
+                        $('#city').html(data);
+                        console.log(data);
+                    }
+                })
+            });
+        </script>
 
 
 
 
 
 
-    <?php
-    include('includes/scripts.php');
-    include('includes/footer.php');
-    ?>
+        <?php
+        include('includes/scripts.php');
+        include('includes/footer.php');
+        ?>
